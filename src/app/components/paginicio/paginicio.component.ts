5b273f9dc3c457470c1d950920d63aa5917cf81c
JUANSOLTEROBARRERA <72166031+JUANSOLTEROBARRERA@users.noreply.github.com>
2022-05-28 10:41:53 -0600

    Chatbot

--- src/app/components/paginicio/paginicio.component.ts
+++ src/app/components/paginicio/paginicio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostDecorator, OnInit } from '@angular/core';
 import { TasksService } from '../../services/tasks.service'
 import { PubService } from '../../services/pub.service';
 import { NgForm } from '@angular/forms'
@@ -10,26 +10,30 @@ import {AuthService} from '../../services/auth.service'
   templateUrl: './paginicio.component.html',
   styleUrls: ['./paginicio.component.css']
 })
+
 export class PaginicioComponent implements OnInit {
 
   tasks = [];
   model: any;
   lcolor: any;
-  
+  horaactual: any;
 
   constructor(private tasksServices: TasksService, public PubServices: PubService, public authService:AuthService) { }
 
   ngOnInit(): void {
     this.model=[
-      {mensaje: ">Baymax: Mi nombre es Baymax. Contestaré a tus preguntas acerca de la plataforma de Ciencia y Tecnología. Si quieres salir, escribe 'salir'"},
-      {mensaje:'>Tú: Hola'},
-      {mensaje: '>Baymax: Hola, ¿Cómo te puedo ayudar?'},
-      {mensaje:'>Tú: ¿Puedo publicar mi artículo aquí?'},
-      {mensaje: '>Baymax: Cualquier persona puede publicar sus artículos en esta plataforma.'},
-      {mensaje:'>Tú: ¿Puedo eliminar un artículo que publiqué?'},
-      {mensaje: '>Baymax: Puedes eliminar tus publicaciones yendo a la sección de mis publicaciones con la cuenta iniciada.'},
-      {mensaje:'>Tú: ¿Quién estuvo detrás del desarrollo de esta plataforma?'},
-      {mensaje: '>Baymax: Los nombres de los ingenieros que estuvieron detrás del desarrollo de esta plataforma se encuentran en la sección de acerca de nosotros.'}
+      {mensaje: "Mi nombre es Baymax. Contestaré a tus preguntas acerca de la plataforma de Ciencia y Tecnología. Si quieres salir, escribe 'salir'."},
+      {mensaje:'Hola, ¿Puedes ayudarme?'},
+      {mensaje: 'Hola, ¿Cómo te puedo ayudar?'},
+      {mensaje:'¿Puedo publicar mi artículo aquí?'},
+      {mensaje: 'Cualquier persona puede publicar sus artículos en esta plataforma.'},
+      {mensaje:'¿Puedo eliminar un artículo que publiqué?'},
+      {mensaje: 'Puedes eliminar tus publicaciones yendo a la sección de mis publicaciones con la cuenta iniciada.'},
+      {mensaje:'¿Quién estuvo detrás del desarrollo de esta plataforma?'},
+      {mensaje: 'Los nombres de los ingenieros que estuvieron detrás del desarrollo de esta plataforma se encuentran en la sección de acerca de nosotros.'},
+      this.horaactual = [
+        {horaxd:'10:44'}
+      ]
     ]
     this.lcolor = false;
     this.getPubs();
@@ -42,7 +46,6 @@ export class PaginicioComponent implements OnInit {
         err => console.log(err)
       )
   }
-
   resetForm(form: NgForm) {
     form.reset();
   }
@@ -50,7 +53,19 @@ export class PaginicioComponent implements OnInit {
     this.lcolor=!this.lcolor;
     return this.lcolor;
   }
-
+  addZero(i:any) {
+    if (i < 10) {i = "0" + i}
+    return i;
+  }
+  damehora(){
+    var today = new Date();
+    let m = this.addZero(today.getMinutes());
+    var time = today.getHours() + ":" + m;
+    this.horaactual = [
+      {horaxd:time}
+    ]
+    return true
+  }
   getPubs() {
     this.PubServices.getPubs().subscribe(
       res => {
